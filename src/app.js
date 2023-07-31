55617eface952f30b549fd061de13b53ef03dad9
Ostap <Ostapkriven13@gmail.com>
2023-07-31 17:13:08 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
+
 import './style.css';
 import { fetchBreeds } from "./cat-api";
-
 
 const errorItem = document.querySelector('.error');
 const loaderItem = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
-
 
 errorItem.textContent = '';
 errorItem.style.display = 'none';
@@ -14,31 +13,36 @@ select.style.visibility = 'hidden';
 
 let storedBreeds = [];
 
+function onLoader() {
+    loaderItem.style.display = 'block';
+    fetchBreeds()
+        .then((data) => {
+            data = data.filter(img => img.image?.url != null)
+            storedBreeds = data;
 
-fetchBreeds()
-    .then((data) => {
-        data = data.filter(img => img.image?.url != null)
-        storedBreeds = data;
-    
-        for (let i = 0; i < storedBreeds.length; i++) {
-            const breed = storedBreeds[i];
-            if (!breed.image) continue;
-            let option = document.createElement('option');
-            option.value = breed.id;
-            option.innerHTML = `${breed.name}`;
-            select.appendChild(option);
-        }
-        
+            for (let i = 0; i < storedBreeds.length; i++) {
+                const breed = storedBreeds[i];
+                if (!breed.image) continue;
+                let option = document.createElement('option');
+                option.value = breed.id;
+                option.innerHTML = `${breed.name}`;
+                select.appendChild(option);
+            }
 
-        select.style.visibility = 'visible';
-    })
-    .catch((error) => console.log(error)); 
-
+            select.style.visibility = 'visible'; 
+            loaderItem.style.display = 'none';
+        })
+        .catch((error) => {
+            loaderItem.style.display = 'none'; 
+            errorMessage();
+            console.error(error);
+        });
+};
 
 function fetchCatByBreed() {
     let breedId = select.value;
     const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-    
+
     return fetch(url, {
         headers: {
             'x-api-key': api_key
@@ -47,18 +51,18 @@ function fetchCatByBreed() {
         .then((response) => {
             if (!response.ok) {
                 throw new Error(response.status);
-            } 
+            }
             return response.json();
         })
         .then((data) => {
             let id = select.selectedIndex
             data = storedBreeds[id];
-            renderBreeds(); 
+            renderBreeds();
         })
         .catch((error) => errorMessage());
 };
 
-function renderBreeds() { 
+function renderBreeds() {
     let id = select.selectedIndex;
 
     if (storedBreeds.length > 1) {
@@ -68,7 +72,6 @@ function renderBreeds() {
     const title = document.createElement('h2');
     const description = document.createElement('p');
     const temperament = document.createElement('h3');
-
 
     image.style.display = 'block';
     image.src = `${storedBreeds[id].image.url}`;
@@ -83,7 +86,7 @@ function renderBreeds() {
 
     description.textContent = `${storedBreeds[id].description}`;
     description.style.fontSize = `${24}px`;
-    
+
     temperament.textContent = `Temperament: ${storedBreeds[id].temperament}`;
     temperament.style.fontSize = `${28}px`;
     temperament.style.marginTop = 0;
@@ -92,21 +95,32 @@ function renderBreeds() {
     catInfo.append(image, title, description, temperament);
 };
 
-
 function errorMessage() {
-    loaderItem.style.display = 'none'; 
+    loaderItem.style.display = 'none';
     loaderItem.textContent = '';
     errorItem.textContent = 'Oops! Something went wrong! Try reloading the page!';
     errorItem.style.display = 'block';
 };
 
-
 select.addEventListener('change', fetchCatByBreed);
+
 
 onLoader();
 
 
-function onLoader() {
-    loaderItem.style.display = 'block'; 
-    fetchCatByBreed(); 
+const breeds = `https://api.thecatapi.com/v1/breeds`;
+const api_key = "live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH"
+
+export function fetchBreeds() {
+    return fetch(breeds, {
+        headers: {
+            'x-api-key': api_key
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
 };
