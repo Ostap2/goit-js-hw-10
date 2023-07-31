defbfd8ca001b7a246f75008c0a02a73a92edba3
Ostap <Ostapkriven13@gmail.com>
2023-07-31 11:29:27 +0300

    s
    s

--- src/app.js
+++ src/app.js
@@ -1,74 +1,139 @@
-import { fetchBreeds, fetchCatsByBreed } from "./cat-api.js";
+const loaderImg = 'url(https://papik.pro/grafic/uploads/posts/2023-04/1682206914_papik-pro-p-stikeri-rizhii-kot-vektor-26.png)';
 
-const breedSelect = document.querySelector(".breed-select");
-const catInfo = document.querySelector(".cat-info");
-const catBreed = document.querySelector(".cat-breed");
-const catList = document.querySelector(".cat-list");
-const loader = document.querySelector(".loader");
-const error = document.querySelector(".error");
-const catDescription = document.querySelector(".cat-description");
-const catTemperament = document.querySelector(".cat-temperament");
+import './css/styles.css';
 
-catInfo.style.display = "none";
-error.style.display = "none";
+import { fetchBreeds } from "./js/cat-api";
 
-function showLoader() {
-  loader.style.display = "block";
-}
+const errorItem = document.querySelector('.error');
+errorItem.textContent = '';
+errorItem.style.display = 'none';
+const loaderItem = document.querySelector('.loader');
 
-function hideLoader() {
-  loader.style.display = "none";
-}
+const catInfo = document.querySelector('.cat-info');
+const select = document.querySelector('.breed-select');
+select.addEventListener('change', fetchCatByBreed);
+select.style.visibility = 'hidden';
 
-function showError() {
-  error.style.display = "block";
-}
-
-function clearCatList() {
-  catList.innerHTML = "";
-}
-
-function populateBreedSelect(breeds) {
-  breedSelect.innerHTML = breeds
-    .map((breed) => `<option value="${breed.id}">${breed.name}</option>`)
-    .join("");
-}
+let storedBreeds = [];
 
 fetchBreeds()
-  .then((breeds) => {
-    populateBreedSelect(breeds);
-  })
-  .catch(() => {
-    showError();
-  });
-
-breedSelect.addEventListener("change", (event) => {
-  const breedId = event.target.value;
-
-  showLoader();
-
-  fetchCatsByBreed(breedId)
-    .then((catData) => {
-      hideLoader();
-
-      if (catData) {
-        catInfo.style.display = "block";
-        catBreed.textContent = catData.breed;
-        catDescription.textContent = catData.description;
-        catTemperament.textContent = catData.temperament;
-
-        const catImage = document.createElement("img");
-        catImage.src = catData.image;
-        catImage.alt = "Cat Image";
-
-        clearCatList();
-        catList.appendChild(catImage);
-      } else {
-        catInfo.style.display = "none";
-      }
+    .then((data) => {
+        data = data.filter(img => img.image?.url != null)
+        storedBreeds = data;
+    
+        for (let i = 0; i < storedBreeds.length; i++) {
+            const breed = storedBreeds[i];
+            let option = document.createElement('option');
+            
+            if (!breed.image) continue
+            option.value = breed.id;
+            option.innerHTML = `${breed.name}`;
+            select.appendChild(option);
+        };
     })
-    .catch(() => {
-      hideLoader();
-      showError();
-    });
-});
+    .catch((error) => console.log(error)); 
+        
+const api_key = `live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH`
+
+function fetchCatByBreed() {
+    let breedId = select.value
+    
+    const url = "https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}"
+    
+    return fetch(url, {
+        headers: {
+            'x-api-key': api_key
+        }
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            } 
+            return response.json();
+        })
+        .then((data) => {
+            let id = select.selectedIndex
+            data = storedBreeds[id];
+        
+            return renderBreeds();
+        })
+        .catch((error) => errorMessage());
+};
+
+function renderBreeds() { 
+    let id = select.selectedIndex;
+
+    if (storedBreeds.length > 1) {
+        catInfo.innerHTML = '';
+    }
+    const image = document.createElement('img');
+    const title = document.createElement('h2');
+    const description = document.createElement('p');
+    const temperament = document.createElement('h3');
+
+    image.style.display = 'block';
+    image.src = `${storedBreeds[id].image.url}`;
+    image.style.width = `${700}px`;
+    image.style.height = `${600}px`;
+    image.style.backgroundSize = 'cover';
+
+    title.textContent = `${storedBreeds[id].name}`;
+    title.style.fontSize = `${38}px`;
+    title.style.marginBottom = 0;
+    title.style.backgroundColor = 'rgb(255, 255, 255)';
+
+    description.textContent = `${storedBreeds[id].description}`;
+    description.style.fontSize = `${24}px`;
+    
+    temperament.textContent = `Temperament: ${storedBreeds[id].temperament}`;
+    temperament.style.fontSize = `${28}px`;
+    temperament.style.marginTop = 0;
+
+    catInfo.append(image, title, description, temperament);
+};
+
+function onLoader() {
+
+    select.style.visibility = 'visible';
+   
+    fetchCatByBreed();
+};
+
+onLoader()
+
+function errorMessage() {
+
+    loaderItem.style.display = 'none';
+    loaderItem.textContent = '';
+    errorItem.textContent = 'Oops! Something went wrong! Try reloading the page!';
+    errorItem.style.display = 'block';
+
+};
+// // errorMessage()
+
+
+// const errorItem = document.querySelector('.error')
+// errorItem.textContent = '';
+// errorItem.style.visibility = 'hidden';
+// console.dir(errorItem);
+
+
+
+
+// catInfo.innerHTML +=`<img src="${image.src}" alt=""><h2>${title.textContent}</h2><p>${description.textContent}</p><h3>${temperament.textContent}</h3>`
+
+
+// const boxUrl = document.createElement('div')
+// errorItem.after(boxUrl);
+// boxUrl.style.display = 'block';
+// boxUrl.style.width = `${700}px`;
+// boxUrl.style.height = `${700}px`;
+// boxUrl.style.backgroundImage = 'url(https://t3.ftcdn.net/jpg/00/61/67/86/240_F_61678606_bNPFDZsyh2tG7Lxjm5Jd0Xd7QMeumObE.jpg)'
+// boxUrl.style.backgroundRepeat = 'no-repeat';
+
+// errorItem.style.display = 'none';
+    // errorItem.textContent = '';
+    // loaderItem.textContent = 'Loading data, please wait...';
+    // loaderItem.style.display = 'block'
+    // loaderItem.classList.replace('error', 'loader');
+    
--- src/cat-api.js
+++ src/cat-api.js
@@ -1,32 +1,86 @@
+const errorItem = document.querySelector('.error');
+// errorItem.hidden = true;
+const loaderItem = document.querySelector('.loader');
+// loaderItem.hidden = false;
+const catInfo = document.querySelector('.cat-info')
 
-import axios from "axios";
-
-axios.defaults.headers.common["x-api-key"] = "твій ключ";
+const breeds = `https://api.thecatapi.com/v1/breeds`;
+// const breed = 'https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}'
+const api_key = "live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH"
 
 export function fetchBreeds() {
-  return axios
-    .get("https://api.thecatapi.com/v1/breeds")
-    .then((response) => response.data)
-    .catch((error) => {
-      throw error;
-    });
-}
+    return fetch(breeds,{headers: {
+    'x-api-key': api_key
+    }})
+        .then((response) => {
+        if (!response.ok) {
+        throw new Error(response.status);
+        }
+        return response.json();
+        })
+};
 
-export function fetchCatsByBreed(breedId) {
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then((response) => {
-      const catData = response.data[0];
-      const catBreedInfo = catData.breeds[0];
 
-      return {
-        image: catData.url,
-        breed: catBreedInfo.name,
-        description: catBreedInfo.description,
-        temperament: catBreedInfo.temperament,
-      };
-    })
-    .catch((error) => {
-      throw error;
-    });
-}
+
+
+
+
+
+
+// https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}
+
+// ?${searchParams}
+        // "breeds": 
+
+        //         "id": "acur",
+        //         "name": "American Curl",
+                
+        //         "temperament": "Affectionate, Curious, Intelligent, Interactive, Lively, Playful, Social",
+             
+        //         "description": "Distinguished by truly unique ears that curl back in a graceful arc, offering an alert, perky, happily surprised expression, they cause people to break out into a big smile when viewing their first Curl. Curls are very people-oriented, faithful, affectionate soulmates, adjusting remarkably fast to other pets, children, and new situations.",
+
+        // "id": "ji-5E0VwY",
+        // "url": "https://cdn2.thecatapi.com/images/ji-5E0VwY.jpg",
+
+
+// +++++++++++++
+// fetch(url,{headers: {
+//     'x-api-key': api_key
+//     }})
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error(resp.statusaText)
+//         }
+//         // console.log(response);
+//         return response.json();
+//     })
+// .then((data) => {
+//    //filter to only include those with an `image` object
+//     data = data.filter(img => img.image?.url!=null)
+// //    console.log(data)
+//     storedBreeds = data;
+//     console.log(storedBreeds);
+//     for (let i = 0; i < storedBreeds.length; i++) {
+//     const breed = storedBreeds[i];
+//     let option = document.createElement('option'); 
+//      //skip any breeds that don't have an image
+//     if(!breed.image)continue
+//     //use the current array index
+//     option.value = i;
+//     option.innerHTML = `${breed.name}`;
+//     select.appendChild(option);
+//     }
+// //    show the first breed by default
+// //    console.log(showBreedImage())
+//     console.dir(select)
+// })
+// .catch(function(error) {
+// //    console.log(error);
+// });
+
+// function showBreedImage(index) {
+//     document.getElementById("#breed_image").src = storedBreeds[index].image.url
+//     document.getElementById("#breed_json").textContent = storedBreeds[index].temperament
+
+// }
+// ++++++++++++++
