11e603f2ea96d64cb1323628c422c6fda8afbde9
Ostap <Ostapkriven13@gmail.com>
2023-08-04 10:20:42 +0300

    s
    s

--- src/cat-api.js
+++ src/cat-api.js
@@ -9,13 +9,18 @@ const breeds = `https://api.thecatapi.com/v1/breeds`;
 export const api_key = "live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH"
 
 export function fetchBreeds() {
-    return fetch(breeds,{headers: {
-    'x-api-key': api_key
-    }})
-        .then((response) => {
+    return fetch(breeds, { headers: { 'x-api-key': api_key } })
+      .then((response) => {
         if (!response.ok) {
-        throw new Error(response.status);
+          if (response.status === 404) {
+            return [{ id: 'malayan', name: 'Malayan' }];
+          } else {
+            throw new Error(response.status);
+          }
         }
         return response.json();
-        })
-};
+      })
+      .catch((error) => {
+        throw new Error(error.message || "Failed to fetch breeds");
+      });
+  }
