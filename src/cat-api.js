921048fec05cf104646ac8002d2ecd0ccbde1c0a
Ostap <Ostapkriven13@gmail.com>
2023-08-04 10:31:27 +0300

    і

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,21 +1,20 @@
+import SlimSelect from 'slim-select';
+
 const errorItem = document.querySelector('.error');
-
 const loaderItem = document.querySelector('.loader');
-
-const catInfo = document.querySelector('.cat-info')
+const catInfo = document.querySelector('.cat-info');
+const select = document.querySelector('.breed-select');
 
 const breeds = `https://api.thecatapi.com/v1/breeds`;
 
-export const api_key = "live_CIw3lZRkcpgh759C9YBXivIvAETipzFqRyXtOHa4sXukf5xIGdNG9JZOQ72DPlKH"
+export const api_key = "live_JN7SGLs0bBlgTEetxzYXuopzP3WrCT2FAmmQQyIkE7YGEUgp1MC2MI3IRIsQ9wYo";
 
 export function fetchBreeds() {
-    return fetch(breeds,{headers: {
-    'x-api-key': api_key
-    }})
+    return fetch(breeds, { headers: { 'x-api-key': api_key } })
         .then((response) => {
-        if (!response.ok) {
-        throw new Error(response.status);
-        }
-        return response.json();
-        })
-};
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+}
